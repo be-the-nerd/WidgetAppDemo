359cf562f34c9888a43ee389a51df5b33a75cc37
be-the-nerd <divyanshverma010@gmail.com>
2022-05-04 00:38:26 +0530

    Widget App Demo + Firebase Analytics

---
+++ app/src/main/java/com/divyansh/widgetsampleapp/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.divyansh.widgetsampleapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.ktx.Firebase
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var firebaseAnalytics: FirebaseAnalytics
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Obtain the FirebaseAnalytics instance.
+        firebaseAnalytics = Firebase.analytics
+    }
+}
---
+++ app/src/main/java/com/divyansh/widgetsampleapp/NewAppWidget.kt
@@ -0,0 +1,50 @@
+package com.divyansh.widgetsampleapp
+
+import android.app.PendingIntent
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.widget.RemoteViews
+
+/**
+ * Implementation of App Widget functionality.
+ */
+class NewAppWidget : AppWidgetProvider() {
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
+        // There may be multiple widgets active, so update all of them
+        for (appWidgetId in appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId)
+        }
+    }
+
+    override fun onEnabled(context: Context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    override fun onDisabled(context: Context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+}
+
+internal fun updateAppWidget(
+    context: Context,
+    appWidgetManager: AppWidgetManager,
+    appWidgetId: Int
+) {
+    val remoteViews = RemoteViews(context.packageName, R.layout.new_app_widget)
+
+    val intent = Intent(Intent.ACTION_VIEW)
+    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+    intent.data = Uri.parse("https://www.android.com")
+
+    val pendingIntent = PendingIntent.getActivity(context, 0, intent, 0)
+    remoteViews.setOnClickPendingIntent(R.id.openWebButton, pendingIntent)
+
+    appWidgetManager.updateAppWidget(appWidgetId, remoteViews)
+}
